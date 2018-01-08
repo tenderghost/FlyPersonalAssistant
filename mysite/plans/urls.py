d39cb31045181edc6052377a299c12f11f5e5600
tenderghost <tenderghost@gmail.com>
2018-01-09 00:20:28 +0800

    Add plan index page html

--- mysite/plans/urls.py
+++ mysite/plans/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = 'plans'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.IndexView.as_view(), name='index')
 ]
--- mysite/plans/views.py
+++ mysite/plans/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.views import generic
 from django.http import HttpResponse
+from .models import Plan
+
+
+class IndexView(generic.ListView):
+    template_name = "plans/index.html"
+    context_object_name = "latest_plan_list"
+
+    def get_queryset(self):
+        return Plan.objects.all()
 
 
 def index(request):
