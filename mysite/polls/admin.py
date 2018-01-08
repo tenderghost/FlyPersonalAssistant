c454da4ce945d0fb5bcd4669e26570d991a852ee
tenderghost <tenderghost@gmail.com>
2018-01-08 12:40:07 +0800

    Add django view support according to the great tutorial: https://docs.djangoproject.com/en/2.0/intro/tutorial02/

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 # add django admin site support
 admin.site.register(Question)
+admin.site.register(Choice)
