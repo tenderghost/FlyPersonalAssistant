c145c94774c6a41a639525f75c0e7d9379cca452
tenderghost <tenderghost@gmail.com>
2018-01-08 23:48:59 +0800

    Add static file support, but seems failed.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls'))
+    path('polls/', include('polls.urls')),
+    path('plans/', include('plans.urls'))
 ]
--- mysite/plans/views.py
+++ mysite/plans/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('Plan Management start here!')
