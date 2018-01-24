6e8811f5961e02e0169e5b03eb0c09025d5234a9
tenderghost <tenderghost@gmail.com>
2018-01-24 21:19:25 +0800

    Add sensors server code

--- mysite/sensors/models.py
+++ mysite/sensors/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class TemHem(models.Model):
     """
     A data contains temperature and humidity
+    Contains only original data
     """
     data_time = models.DateTimeField(auto_now=True)
     temperature = models.FloatField()
